3e4aff127ed2e395c73b1ca35a122b010aabf221
최규광 <kisay97@naver.com>
2016-05-24 16:43:14 +0900

    Initial commit

---
+++ app/src/main/java/com/estsoft/getresult/CalleeActivity.java
@@ -0,0 +1,27 @@
+package com.estsoft.getresult;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CalleeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_callee);
+
+        findViewById(R.id.finishButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data-int", 10);
+                intent.putExtra("data-string", "android");
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
